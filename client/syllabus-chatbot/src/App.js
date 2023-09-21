7f26c791298e585f3f41dd3eb57a8e5773fdcdc9
Vsri03 <vxs200046@utdallas.edu>
2023-09-21 17:47:01 -0500

    Added simple chatbot features

---
+++ client/syllabus-chatbot/src/App.js
@@ -0,0 +1,43 @@
+import ChatBot from 'react-simple-chatbot';
+ 
+const steps = [
+  {
+      id: '0',
+      message: 'Hello!',
+
+      // This calls the next id
+      // i.e. id 1 in this case
+      trigger: '1',
+  }, {
+      id: '1',
+
+      // This message appears in
+      // the bot chat bubble
+      message: 'What would you like help with?',
+      trigger: '2'
+  }, {
+      id: '2',
+
+      // Here we want the user
+      // to enter input
+      user: true,
+      trigger: '3'
+  }, {
+    id: '3',
+    message: "Sure, I will help you with the following: {previousValue}",
+  }
+];
+ 
+function App() {
+    return (
+        <div className="App">
+            <h1>Syllabus Chatbot</h1>
+            <ChatBot
+              style= {{height: '100vh', width: '100%'}}
+              steps={steps}
+            />
+        </div>
+    );
+}
+ 
+export default App;
