e694493f392f7d1e882a36478aee04e204386104
Kanishk Garg <kanishkg2010@gmail.com>
2023-10-05 18:42:13 -0500

    openai secure key management

--- server/src/model/openai.py
+++ server/src/model/openai.py
@@ -1,5 +1,13 @@
 import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 class OpenAI:
     def __init__(self):
         pass
+
+if __name__ == '__main__':
+    print(OPENAI_API_KEY)
