5d18f81ff814df1cb16dc3c06801f4c3b7597b9e
Kanishk Garg <kanishkg2010@gmail.com>
2023-10-13 16:27:49 -0500

    load tokenized pdf

--- server/src/model/openai.py
+++ server/src/model/openai.py
@@ -1,9 +1,15 @@
 import openai
 from dotenv import load_dotenv
-import os
+import os, sys
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# load tokenized pdf from ../resources - agnostic from where script called
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+target_file_path = os.path.join(script_directory, '..', 'resources', 'cs4349_chida.txt')
+with open(target_file_path, 'r') as file:
+    SYLLABUS_TEXT = file.read()
 
 class OpenAI:
     def __init__(self):
@@ -11,3 +17,4 @@ class OpenAI:
 
 if __name__ == '__main__':
     print(OPENAI_API_KEY)
+    print(SYLLABUS_TEXT)
