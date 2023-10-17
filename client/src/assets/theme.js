b9e173e37acf6c14507d77771b18d9e8dc3ca8ef
aryankapoorr <aryan.kapoor.1102@gmail.com>
2023-10-17 15:43:36 -0500

    💄 utd themed chatbot styling

---
+++ client/src/assets/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    background: '#e8eceb',
+    fontFamily: 'Helvetica Neue',
+    headerBgColor: '#f39036',
+    headerFontColor: '#fff',
+    headerFontSize: '15px',
+    botBubbleColor: '#ed9133',
+    botFontColor: '#fff',
+    userBubbleColor: '#46695d',
+    userFontColor: '#ffffff',
+};  
--- client/src/components/chatbot.js
+++ client/src/components/chatbot.js
@@ -1,9 +1,13 @@
-// Imports chatbot component's library
+// Imports chatbot & theme component libraries
 import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
 
 // Preset options for students to select
 // Contains basic syllabus information
 import { steps } from '../assets/steps';
+
+// Contains the theme and styling of the chatbot
+import { theme } from '../assets/theme';
 
 // Syllabus chatbot avatar
 import temocLogo from '../assets/temoc.png';
@@ -11,14 +15,16 @@ import temocLogo from '../assets/temoc.png';
 function ChatbotComponent() {
     return (
         <div className="App">
-            <ChatBot
-                headerTitle="Advanced Algorithms Syllabus Chatbot"
-                contentStyle={{ height: '85vh' }}
-                style={{ height: '100%', width: '100%' }}
-                steps={steps}
-                botAvatar={temocLogo}
-                hideUserAvatar={true}
-            />
+            <ThemeProvider theme={theme}>
+                <ChatBot
+                    headerTitle="Advanced Algorithms Syllabus Chatbot"
+                    contentStyle={{ height: '85vh' }}
+                    style={{ height: '100%', width: '100%' }}
+                    steps={steps}
+                    botAvatar={temocLogo}
+                    hideUserAvatar={true}
+                />
+            </ThemeProvider>
         </div>
     );
 }
