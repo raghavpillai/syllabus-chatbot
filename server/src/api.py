2bc89d8fdfc69c04155dbbe1e0c6d4952b89fe73
raghavpillai <raghavpillai101@gmail.com>
2023-10-19 16:32:53 -0500

    Added single completion to openAI

--- server/src/api.py
+++ server/src/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from typing import Dict, Any
 
 from src.util.response import Response
-from src.model.openai_module import OpenAI
+from server.src.model.openai_model import OpenAI
 
 app: FastAPI = FastAPI()
 
@@ -36,5 +36,13 @@ async def main(messages: list[Dict]) -> Dict[str, Any]:
     res: Response = Response(
         success=True,
         message=OpenAI.get_completion(messages)
+    )
+    return res.response()
+
+@app.get(f"{OPENAI_V1_ENDPOINT}/get_response")
+async def get_response(message: str) -> Dict[str, Any]:
+    res: Response = Response(
+        success=True,
+        message=OpenAI.get_single_completion(message)
     )
     return res.response()
---
+++ server/src/model/openai_model.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import openai
+from dotenv import load_dotenv
+from typing import List, Dict, Any
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+openai.api_key = OPENAI_API_KEY
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+RESOURCE_PATH = os.path.join(CURRENT_DIR, '..', 'resources')
+
+class OpenAI:
+    @staticmethod
+    def get_completion(messages: list[dict]):
+        
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        return completion.choices[0].message.content
+    
+    @classmethod
+    def get_single_completion(cls, message: str) -> str:
+        cls.messages.append({"role": "user", "content": message})
+        completion: openai.Completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=cls.messages
+        )
+        return completion.choices[0].message.content
+    
+    @classmethod
+    def initialize(cls) -> None:
+        cls.messages: List[Dict[str, str]] = [{"role": "system", "content": "You are a useful teacher's assistant for an advanced algorithm's class"}]
+        with open(f'{RESOURCE_PATH}/cs4349_chida.txt', 'r') as file:
+            cls.syllabus_text = file.read()
+
+
+if __name__ == '__main__':
+    print(OPENAI_API_KEY)
+    print(OpenAI.initialize())
+    # print(OpenAI.get_completion([{"role": "user", "content": "whats merge sort"}]))
